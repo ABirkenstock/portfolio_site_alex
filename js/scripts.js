ed83184dc96a2e9fa6b7c65c28cdbd2fcb5e9922
ABirkenstock <davidbirkenstock@gmx.de>
2023-12-02 14:09:08 +0100

    mobile nav toggle function

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,9 @@
 
-console.log("Hey there")
+function menuToggle() {
+    var x = document.getElementById('myNavtoggle');
+    if (x.className === 'navtoggle') {
+      x.className += ' responsive';
+    } else {
+      x.className = 'navtoggle';
+    }
+  }
